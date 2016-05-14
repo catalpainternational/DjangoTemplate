bd45ae9e15475d5764fcc522ac9bbb2fbd5a2825
Anders Hofstee <a.hofstee@catalpainternational.org>
2016-05-14 10:38:11 +0900

    adds internationalisation support and rosetta

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+                            url(r'^rosetta/', include('rosetta.urls')),
+                            )
