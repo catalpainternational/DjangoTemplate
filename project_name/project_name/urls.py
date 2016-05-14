113a8f24ae6c44e3c0f59dc0ac34ce6f6f7d7f52
Anders Hofstee <a.hofstee@catalpainternational.org>
2016-05-14 12:07:53 +0900

    fix misbehaving

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -28,7 +28,7 @@ router.register(r'mymodels', views.MyModelViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', IndexView.as_view(), name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
