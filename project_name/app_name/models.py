70764f8589e92590a01be94abb3cd5c544001226
ony <dejesushonorio03@gmail.com>
2016-05-14 10:44:15 +0900

    fixed models field

--- project_name/app_name/models.py
+++ project_name/app_name/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 # Create your models here.
 
-class Appname(models.Mode):
-    text = models.CharFied(max_length=100)
+class Appname(models.Model):
+    text = models.CharField(max_length=100)
