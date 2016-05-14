75dee7e61ec65c3c4fb7e4a58b728ef51179ca8d
ony <dejesushonorio03@gmail.com>
2016-05-14 10:35:35 +0900

    making models and urls files

--- project_name/app_name/models.py
+++ project_name/app_name/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Appname(models.Mode):
+    text = models.CharFied(max_length=100)
--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app_name',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'PAGE_SIZE': 300
+}
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
